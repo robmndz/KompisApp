903007f1d44b787dc12dc305d63a23bd48f2a6ef
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-04-09 12:37:31 +0200

    Adding different RequestParam

--- src/main/java/com/example/spring_initializr2/HelloWorldController.java
+++ src/main/java/com/example/spring_initializr2/HelloWorldController.java
@@ -1,14 +1,33 @@
 package com.example.spring_initializr2;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloWorldController {
 
     @RequestMapping("/")
     public String index() {
-        return "Välkommen till kompis agenda";
+        return "Hello World";
     }
 
+    @RequestMapping("/list")
+    public List<String> hello() {
+        return List.of("Hello", "World");
+    }
+
+    @RequestMapping("/listHello")
+    public String listHello(@RequestParam List<String> id) {
+        return "IDs are " + id;
+    }
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String firstName,
+                        @RequestParam String lastName) {
+
+        return ("Hello " + firstName + " " + lastName);
+    }
 }
